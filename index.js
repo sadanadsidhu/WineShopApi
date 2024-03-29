ce247f7dbd9fd0c4bd4f180e2b02559e040fe6f3
sadanand kumar <sadanandsidhu@gmail.com>
2024-03-29 10:23:18 +0530

    selfie with status merged

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use("/", require("./routes/uploadImageRoutes"));
-app.use("/", require("./routes/winesCategoriesRoutes"));
-app.use("/", require("./routes/wineShopRoutes"));
-app.use("/", require("./routes/userOtpVarificationRoutes"));
-app.use("/", require("./routes/customerRegisterRoutes"));
-app.use("/", require("./routes/uploadAadharroutes"));
-app.use("/", require("./routes/uploadPanRoutes"));
-app.use("/", require("./routes/aadharFetchDataRoutes"));
-app.use("/", require("./routes/panCardFetchDataRoutes"));
-app.use("/", require("./routes/uploadSelfieImages"));
-app.use("/", require("./routes/selfieImagesWithStatusRoutes"));
+app.use('/', require("./routes/uploadImageRoutes"));
+app.use('/', require("./routes/winesCategoriesRoutes"));
+app.use('/', require("./routes/wineShopRoutes"));
+app.use('/', require("./routes/userOtpVarificationRoutes"));
+app.use('/', require("./routes/customerRegisterRoutes"));
+app.use('/', require("./routes/uploadAadharroutes"));
+app.use('/', require("./routes/uploadPanRoutes"));
+app.use('/', require("./routes/aadharFetchDataRoutes"));
+app.use('/', require("./routes/panCardFetchDataRoutes"));
+app.use('/', require("./routes/uploadSelfieImages"));
+app.use('/', require("./routes/selfieImagesWithStatusRoutes"));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
