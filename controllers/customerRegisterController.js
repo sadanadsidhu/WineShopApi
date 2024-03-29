ce5731aa3d23820423b3e2a2b3b6af837eac5aec
SARATHLAL HS <sarathlalsh@gmail.com>
2024-03-30 01:10:07 +0530

    selfie Status

--- controllers/customerRegisterController.js
+++ controllers/customerRegisterController.js
@@ -1,111 +1,86 @@
-const { response } = require('express');
-const Customer = require('../models/customberRegisterModel');
-// const cookie = require('cookie-parser');
+const Customer = require("../models/customberRegisterModel");
 
-// const jwt = require('jsonwebtoken'); 
-// require('dotenv').config();
+/////////////////register customer-------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// const createUser = async (req, res) => {
 
-// const generateAccessAndRefreshToken = async (userid) => {
 //   try {
-//     const user = await Customer.findById(userid);
-//     if (!user) {
-//       throw new Error("User not found");
+//     // Your existing createUser code
+//     const { mobileNumber, username, email } = req.body;
+
+//     const existingCustomer = await Customer.findOne({
+
+//       $or: [{ username, email }],
+//     });
+
+//     if (existingCustomer) {
+//       return res
+//         .status(400)
+//         .json({ error: "Username and email already exist" });
 //     }
 
-//     // Generate access token
-//     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+//     const newCustomer = await Customer.create({
+//       mobileNumber,
+//       username,
+//       email,
+//     });
+//     const user = await Customer.findOne({
+//       $or: [{ username }, { email }]
+//     });
+//     if (!user) {
+//       return res.status(404).json({ error: "User doesn't exist" });
+//     }
 
-//     // Generate refresh token
-//     const refreshToken = jwt.sign({ userId: user._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
-
-//     // Update user's refresh token in the database
-//     user.refreshToken = refreshToken;
-//     await user.save({ validateBeforeSave: false });
-
-//     return { accessToken, refreshToken };
 //   } catch (error) {
 //     console.error(error);
-//     throw new Error("Something went wrong");
+//     return res.status(500).json({ error: "Internal Server Error" });
 //   }
 // };
 
-/////////////////register customer-------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const createUser = async (req, res) => {
-
   try {
-    // Your existing createUser code
     const { mobileNumber, username, email } = req.body;
 
+    // Check if user with the provided username or email already exists
     const existingCustomer = await Customer.findOne({
-      
-      $or: [{ username, email }],
+      $or: [{ username }, { email }],
     });
-    
+
     if (existingCustomer) {
       return res
         .status(400)
-        .json({ error: "Username and email already exist" });
+        .json({ error: "Username or email already exists" });
     }
 
+    // Create a new customer
     const newCustomer = await Customer.create({
       mobileNumber,
       username,
       email,
     });
-    const user = await Customer.findOne({
-      $or: [{ username }, { email }]
-    });
-    if (!user) {
-      return res.status(404).json({ error: "User doesn't exist" });
-    }
-    // const { accessToken, refreshToken, error } = await generateAccessAndRefreshToken(user._id);
-    // if (error) {
-    //   return res.status(500).json({ error });
-    // }
-  
-    // const loggedinUser = await Customer.findById(user._id).select("-refreshToken");
-  
-    // const options = {
-    //   httpOnly: true,
-    //   secure: true,
-    // };
-    
-    // return res.status(200)
-    //   .cookie("accessToken", accessToken, options)
-    //   .cookie("refreshToken", refreshToken, options)
-    //   .json({
-    //     user: loggedinUser,
-    //     accessToken: accessToken,
-    //     refreshToken: refreshToken,
-    //     message: "User register in successfully"
-    //   });
-    // Return success response if needed
-    // return res
-    //   .status(200)
-    //   .json({ message: "User created successfully", user: newCustomer });
+
+    // Optionally, you can return the newly created user
+    return res.status(201).json(newCustomer);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-
 //////////////////////////---------login
 const loginUser = async (req, res) => {
-  const { username, email  } = req.body;
+  const { username, email } = req.body;
   if (!username || !email) {
     return res.status(400).json({ error: "Username and email are required" });
   }
-
 };
 
-/////////////////////get User 
+/////////////////////get User
 
 const getUsersByMobileNumber = async (req, res) => {
   try {
     const { mobileNumber } = req.params;
     const users = await Customer.find({ mobileNumber });
-    
+
     return res.status(200).json({ users });
   } catch (error) {
     console.error(error);
@@ -113,7 +88,7 @@ const getUsersByMobileNumber = async (req, res) => {
   }
 };
 
-module.exports = { 
+module.exports = {
   createUser,
   loginUser,
   getUsersByMobileNumber,
